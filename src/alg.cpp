154cdbb0f4438fe0504c420f6ec968eb81706bf3
LyubimtsevSemyon <99214966+LyubimtsevSemyon@users.noreply.github.com>
2022-06-05 15:16:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,7 +9,7 @@ std::vector<char> getPerm(const Tree& tree, int t) {
   std::string source = tree[t - 1];
   std::vector<char> perm;
   for (int c = 0; c < source.length(); c++) {
-    perm.push_back(source[i]);
+    perm.push_back(source[c]);
   }
   return perm;
 }
